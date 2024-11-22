765d37914f019e4460db9e57c4774412d53f4551
Anupa <anupa@macbookpro.home>
2024-11-22 01:36:52 -0500

    Implementation and functionality of mouse events

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 //mouse events
-
+const hoverButton = document.getElementById('hover-btn');
+const hoverMessage = document.getElementById('change-msg');
+//mouse hovering
+hoverButton.addEventListener('mouseenter', () => {
+    hoverMessage.textContent = "You are hovering on the button!";
+});
+//mouse not hovering
+hoverButton.addEventListener('mouseleave', () => {
+    hoverMessage.textContent = "You are not hovering on the button!";
+});
 
 //keyboard events
 
